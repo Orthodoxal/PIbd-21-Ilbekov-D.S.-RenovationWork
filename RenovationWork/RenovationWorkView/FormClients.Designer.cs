83430ee5ce20ea6c3e3c3a7b89d3a2dc573d4af0
Orthodoxal <dimariktron@mail.ru>
2022-04-20 22:05:08 +0400

    change_name_form

--- RenovationWork/RenovationWorkView/FormClients.Designer.cs
+++ RenovationWork/RenovationWorkView/FormClients.Designer.cs
@@ -38,21 +38,19 @@ namespace RenovationWorkView
             // dataGridView
             // 
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(12, 13);
-            this.dataGridView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView.Location = new System.Drawing.Point(10, 10);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.RowHeadersWidth = 51;
             this.dataGridView.RowTemplate.Height = 25;
-            this.dataGridView.Size = new System.Drawing.Size(469, 556);
+            this.dataGridView.Size = new System.Drawing.Size(410, 417);
             this.dataGridView.TabIndex = 6;
             // 
             // ButtonUpd
             // 
             this.ButtonUpd.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ButtonUpd.Location = new System.Drawing.Point(535, 296);
-            this.ButtonUpd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ButtonUpd.Location = new System.Drawing.Point(468, 222);
             this.ButtonUpd.Name = "ButtonUpd";
-            this.ButtonUpd.Size = new System.Drawing.Size(114, 51);
+            this.ButtonUpd.Size = new System.Drawing.Size(100, 38);
             this.ButtonUpd.TabIndex = 11;
             this.ButtonUpd.Text = "Update";
             this.ButtonUpd.UseVisualStyleBackColor = true;
@@ -61,10 +59,9 @@ namespace RenovationWorkView
             // ButtonDel
             // 
             this.ButtonDel.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ButtonDel.Location = new System.Drawing.Point(535, 238);
-            this.ButtonDel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ButtonDel.Location = new System.Drawing.Point(468, 178);
             this.ButtonDel.Name = "ButtonDel";
-            this.ButtonDel.Size = new System.Drawing.Size(114, 51);
+            this.ButtonDel.Size = new System.Drawing.Size(100, 38);
             this.ButtonDel.TabIndex = 10;
             this.ButtonDel.Text = "Delete";
             this.ButtonDel.UseVisualStyleBackColor = true;
@@ -72,14 +69,15 @@ namespace RenovationWorkView
             // 
             // FormClients
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 577);
+            this.ClientSize = new System.Drawing.Size(602, 433);
             this.Controls.Add(this.ButtonUpd);
             this.Controls.Add(this.ButtonDel);
             this.Controls.Add(this.dataGridView);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormClients";
-            this.Text = "FormClients";
+            this.Text = "Clients";
             this.Load += new System.EventHandler(this.FormClients_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
