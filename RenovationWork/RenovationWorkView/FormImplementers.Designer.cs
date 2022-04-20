60811053d5ff6fbdf3bbfcec85c416eda4245921
Orthodoxal <dimariktron@mail.ru>
2022-04-20 22:04:01 +0400

    no_forms2

--- RenovationWork/RenovationWorkView/FormImplementers.Designer.cs
+++ RenovationWork/RenovationWorkView/FormImplementers.Designer.cs
@@ -29,10 +29,17 @@ namespace RenovationWorkView
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormImplementers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormImplementers";
+            this.Name = "FormImplementers";
+            this.Text = "Implementers";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
